a0f5de1d733b70fc669e223c51fe4b4f1c4ad88f
hikinokouji <steamon1me@gmail.com>
2025-06-21 22:33:16 +0300

    init

---
+++ src/main/java/sta/cfbe/CoffeFlashBackendApplication.java
@@ -0,0 +1,13 @@
+package sta.cfbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeFlashBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoffeFlashBackendApplication.class, args);
+    }
+
+}
