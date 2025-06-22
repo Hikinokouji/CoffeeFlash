658dd2e54b5e7370d60fdc092fed8f36a88f8453
hikinokouji <steamon1me@gmail.com>
2025-06-22 03:49:50 +0300

    checking update in logs

--- src/main/java/sta/cfbe/CoffeFlashBackendApplication.java
+++ src/main/java/sta/cfbe/CoffeFlashBackendApplication.java
@@ -2,12 +2,22 @@ package sta.cfbe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping()
 @SpringBootApplication
 public class CoffeFlashBackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CoffeFlashBackendApplication.class, args);
+    }
+
+    @GetMapping("/api")
+    public void hello(){
+        System.out.printf("Hello World! im workking");
     }
 
 }
