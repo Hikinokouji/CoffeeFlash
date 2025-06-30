a941287782ea2a61ea8eed6f5f60cd1aaedca801
hikinokouji <steamon1me@gmail.com>
2025-06-30 23:58:11 +0300

    LiquiBase: - Creating resource - create Service for update tenant

---
+++ src/main/java/sta/cfbe/service/LiquibaseService.java
@@ -0,0 +1,41 @@
+package sta.cfbe.service;
+
+import liquibase.Contexts;
+import liquibase.Liquibase;
+import liquibase.database.Database;
+import liquibase.database.DatabaseFactory;
+import liquibase.database.jvm.JdbcConnection;
+import liquibase.exception.LiquibaseException;
+import liquibase.resource.ClassLoaderResourceAccessor;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Service;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+@Service
+@RequiredArgsConstructor
+public class LiquibaseService {
+    private final Environment environment;
+
+    public void runLiquibaseForTenant(String tenantDbUrl) throws SQLException, LiquibaseException {
+        String username = environment.getProperty("spring.datasource.username");
+        String password = environment.getProperty("spring.datasource.password");
+        String rawUrl = environment.getProperty("test.liquibase.url");
+        String tenantUrl = rawUrl.replace("{db}", tenantDbUrl);
+
+        Database database = DatabaseFactory.getInstance().findCorrectDatabaseImplementation(
+                new JdbcConnection(DriverManager.getConnection(tenantUrl, username, password))
+        );
+
+        Liquibase liquibase = new Liquibase(
+                "db/master-tenant.yml",
+                new ClassLoaderResourceAccessor(),
+                database
+        );
+
+        liquibase.update(new Contexts());
+    }
+}
