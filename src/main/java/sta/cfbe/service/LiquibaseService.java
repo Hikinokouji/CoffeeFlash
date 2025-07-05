b12ad91cf5c48f1b984be8cb9a8a9aabb55f4737
hikinokouji <steamon1me@gmail.com>
2025-07-05 19:10:28 +0300

    fix: - database connector for docker compose

--- src/main/java/sta/cfbe/service/LiquibaseService.java
+++ src/main/java/sta/cfbe/service/LiquibaseService.java
@@ -6,10 +6,13 @@ import liquibase.database.Database;
 import liquibase.database.DatabaseFactory;
 import liquibase.database.jvm.JdbcConnection;
 import liquibase.exception.LiquibaseException;
+import liquibase.integration.spring.SpringResourceAccessor;
 import liquibase.resource.ClassLoaderResourceAccessor;
+import liquibase.resource.ResourceAccessor;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.core.env.Environment;
+import org.springframework.core.io.DefaultResourceLoader;
 import org.springframework.stereotype.Service;
 
 import java.sql.DriverManager;
@@ -20,19 +23,47 @@ import java.sql.SQLException;
 public class LiquibaseService {
     private final Environment environment;
 
-    public void runLiquibaseForTenant(String tenantDbUrl) throws SQLException, LiquibaseException {
+//    public void runLiquibaseForTenant(String tenantDbUrl) throws SQLException, LiquibaseException {
+//        String username = environment.getProperty("spring.datasource.username");
+//        String password = environment.getProperty("spring.datasource.password");
+//        String rawUrl = environment.getProperty("tenant.db-url-template");
+//        String tenantUrl = rawUrl.replace("{db}", tenantDbUrl);
+//
+//        Database database = DatabaseFactory.getInstance().findCorrectDatabaseImplementation(
+//                new JdbcConnection(DriverManager.getConnection(tenantUrl, username, password))
+//        );
+//
+//        Liquibase liquibase = new Liquibase(
+//                "liquibase/master-tenant.yml",
+//                new ClassLoaderResourceAccessor(),
+//                database
+//        );
+//
+//        liquibase.update(new Contexts());
+//    }
+
+    public void runLiquibaseForTenant(String tenantDbName) throws SQLException, LiquibaseException {
         String username = environment.getProperty("spring.datasource.username");
         String password = environment.getProperty("spring.datasource.password");
         String rawUrl = environment.getProperty("tenant.db-url-template");
-        String tenantUrl = rawUrl.replace("{db}", tenantDbUrl);
 
-        Database database = DatabaseFactory.getInstance().findCorrectDatabaseImplementation(
-                new JdbcConnection(DriverManager.getConnection(tenantUrl, username, password))
-        );
+        if (rawUrl == null) {
+            throw new IllegalStateException("Missing property: tenant.db-url-template");
+        }
+
+        String tenantUrl = rawUrl.replace("{db}", tenantDbName);
+        //log.info("Running Liquibase for tenant DB: {}", tenantUrl);
+
+        Database database = DatabaseFactory.getInstance()
+                .findCorrectDatabaseImplementation(
+                        new JdbcConnection(DriverManager.getConnection(tenantUrl, username, password))
+                );
+
+        ResourceAccessor resourceAccessor = new SpringResourceAccessor(new DefaultResourceLoader());
 
         Liquibase liquibase = new Liquibase(
-                "liquibase/master-tenant.yml",
-                new ClassLoaderResourceAccessor(),
+                "classpath:liquibase/master-tenant.yml",
+                resourceAccessor,
                 database
         );
 
